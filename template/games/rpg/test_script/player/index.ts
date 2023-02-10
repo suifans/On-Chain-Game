a950e08ae23dc2b95f48a36a5db16eeb9fd79cd9
henryliu <17600047539@163.com>
2023-02-10 16:02:45 +0800

    update

---
+++ template/games/rpg/test_script/player/index.ts
@@ -0,0 +1,11 @@
+import {Ed25519Keypair, JsonRpcProvider, RawSigner, TypeTag} from '@mysten/sui.js';
+import {fromExportedKeypair} from "@mysten/sui.js";
+import {ExportedKeypair} from "@mysten/sui.js/src/cryptography/keypair";
+const provider = new JsonRpcProvider();
+const schema = new Ed25519Keypair().getKeyScheme();
+const private_key = 'QVg8OT8vnrT/JdXLHpV3wiBhR1FcdHmamoac5IE2PF+15p+mCrEzOnYixuwiaUOCyUl9emkEbvYoXka++RHEQQ=='
+const key_pair_struct:ExportedKeypair = {
+    schema,
+    privateKey:private_key
+}
+const keypair = fromExportedKeypair(key_pair_struct)
