8525731f1f54ad58e8359ef19caaec979678f653
henryliu <17600047539@163.com>
2023-02-14 15:51:03 +0800

    update

--- template/games/rpg/test_script/Constants/index.ts
+++ template/games/rpg/test_script/Constants/index.ts
@@ -1,8 +1,8 @@
 const packageObjectId = '0x21872eab1d01d73bc1bddc38a1e761780281ff08'
-const mapObjectId = '0x26348ceda7c2f3fab8e3038c0f41eb675e0567e1'
-const monsterObjectId = '0x5e6fc70fcd0d728c4f845f4a49f958e135109463'
-const itemsInfoObjectId = '0x48990e899ada81195dff62d5e748a52bf161a378'
-const playerRulesObjectId = '0xfee595f64b9cecf3537f2b68e6ecad8867fd2581'
+const mapObjectId = '0x8e469761960fb6572a34699f3c2e6f2ed105ce80'
+const monsterObjectId = '0x0af2c8a97e61f3e535be6095565b809665b6cde4'
+const itemsInfoObjectId = '0x2f4027f4926a7bdc957809ab1a57a123f0430b39'
+const playerRulesObjectId = '0x9889d5dcdf9b4ad11a7ead9a31eee03fac5923f2'
 const playerObjectId = '0xb4b04cb755abe7f27d1f6c950434e1bf93fa331e'
 export {
     packageObjectId,
